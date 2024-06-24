02c01a73e67d344792dc96c9f67afa4cc30698de
Scarlett <hao.sijia04@gmail.com>
2024-06-24 11:45:02 -0400

    refactor game status control

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessesList from "../GuessesList/GuessesList";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 export const answer = sample(WORDS);
@@ -14,7 +15,7 @@ function Game() {
   const [guesses, setGuesses] = React.useState([]);
 
   const isWon = guesses.includes(answer);
-  const isDisabled = guesses.length >= 6;
+  const isDisabled = guesses.length >= NUM_OF_GUESSES_ALLOWED; // boolean value
 
   function handleAddGuess(tentativeGuess) {
     setGuesses([...guesses, tentativeGuess]);
