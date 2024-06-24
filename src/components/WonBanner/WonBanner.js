4ccd3b01d38feb50c4f1d5c7488c1e07cdb73e5d
Scarlett <hao.sijia04@gmail.com>
2024-06-24 12:03:37 -0400

    modify syntax error

--- src/components/WonBanner/WonBanner.js
+++ src/components/WonBanner/WonBanner.js
@@ -4,7 +4,7 @@ function WonBanner({numOfGuesses}) {
   return <div class="happy banner">
   <p>
     <strong>Congratulations!</strong> Got it in
-    <strong> {`numOfGuesses === 1 ? '1 guess' : ${numOfGuesses} guesses`}</strong>.
+    <strong> {numOfGuesses === 1 ? '1 guess' : `${numOfGuesses} guesses`}</strong>.
   </p>
 </div>;
 }
